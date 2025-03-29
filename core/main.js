37e9d8cdb0286eb8b12d136e4258176d81aa96d4
Bindi <bindin42@gmail.com>
2025-03-28 19:39:46 -0700

    Adding files

---
+++ core/main.js
@@ -0,0 +1,15 @@
+import { GameEngine } from "./gameengine.js"
+import { AssetManager } from "./assetmanager.js"
+
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.downloadAll(() => {
+	const canvas = document.getElementById("gameWorld");
+	const ctx = canvas.getContext("2d");
+
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+});
